0a85bcc35090b3379f4059958517f5c5e8d84fbb
Toking42 <email@bielefeldts.de>
2015-10-21 12:04:30 +0200

    Create snipplets.js

---
+++ snipplets.js
@@ -0,0 +1,19 @@
+// Builder from tutorial
+
+	if(creep.memory.role == 'builder') {
+		
+			if(creep.carry.energy == 0) {
+				if(Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(Game.spawns.Spawn1);				
+				}
+			}
+			else {
+				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+				if(targets.length) {
+					if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+						creep.moveTo(targets[0]);					
+					}
+				}
+			}
+		}
+
